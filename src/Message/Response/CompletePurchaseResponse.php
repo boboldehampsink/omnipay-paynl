d43e3f9a64378bb147a171d6aa070eda4378b5fa
Bob Olde Hampsink <378974+boboldehampsink@users.noreply.github.com>
2022-08-25 11:42:34 +0200

    Only complete purchase after payment

--- src/Message/Response/CompletePurchaseResponse.php
+++ src/Message/Response/CompletePurchaseResponse.php
@@ -6,5 +6,11 @@ namespace Omnipay\Paynl\Message\Response;
 
 class CompletePurchaseResponse extends FetchTransactionResponse
 {
-
+    /**
+     * @return bool
+     */
+    public function isSuccessful()
+    {
+        return parent::isSuccessful() && $this->isPaid();
+    }
 }
